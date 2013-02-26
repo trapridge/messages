d8f273a4997a40b85f77bf93681fdd92cedb7c0c
Juuso Ansaharju <jansaharju@jmba.kielikone.local>
2013-02-26 21:10:46 +0200

    Added package.json, cleaned up, added deleting of messages

--- server.js
+++ server.js
@@ -1,42 +1,63 @@
-var mongoose = require('mongoose/');
+var mongoose = require('mongoose');
 var config = require('./config');
+var restify = require('restify');
+
+// setup mongoose & mongodb
 mongoose.connect(config.creds.mongoose_auth);
 
-var restify = require('restify');  
+// create a mongoose schema to register a model with mongodb
+var MessageModel = mongoose.model('Message', new mongoose.Schema({
+    message: String,
+    date: Date
+})); 
+
+// setup & start server
 var server = restify.createServer();
 server.use(restify.bodyParser());
 
-// Create a schema for our data
-var MessageSchema = new mongoose.Schema({
-    message: String,
-    date: Date
+server.get('/messages', getMessages);
+server.post('/messages', postMessage);
+server.del('/messages/:id', deleteMessage);
+
+server.listen(8080, function() {
+    console.log('%s listening at %s', server.name, server.url);
 });
 
-// Use the schema to register a model with MongoDb
-mongoose.model('Message', MessageSchema); 
-var Message = mongoose.model('Message'); 
-
+// request handlers
 function getMessages(req, res, next) {
-    Message.find().sort('date asc').execFind(function (arr,data) {
+    MessageModel.find().sort('date asc').execFind(function (arr,data) {
         res.send(data);
     });
 }
 
 function postMessage(req, res, next) {
-    var message = new Message();
-
-console.log(req.params)
-
-    message.message = req.params.message;
-    message.date = new Date();
-    message.save(function () {
+    var newMessage = new MessageModel();
+    newMessage.message = req.params.message;
+    newMessage.date = new Date();
+    newMessage.save(function () {
         res.send(req.body);
     });
 }
 
-server.get('/messages', getMessages);
-server.post('/messages', postMessage);
-
-server.listen(8080, function() {
-    console.log('%s listening at %s, love & peace', server.name, server.url);
-});
+function deleteMessage(req, res, next) {
+    MessageModel.findById(req.params.id, function (err, message) {
+        if (err) {
+            console.log(err);
+            res.send(500);  
+        } 
+        if (message) {
+            var id = message._id;
+            message.remove(function (err, removed) {
+                if (err) {
+                    console.log(err);
+                    res.send(500); 
+                }
+                console.log('Removed message: ' + id);
+                res.send(204);
+            });
+        }
+        else {
+            res.send(404);
+        }
+    });
+}
