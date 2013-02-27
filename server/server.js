6c4cbe1bf3de328b8619f7d2f49530ce33c7d9f3
Juuso Ansaharju <jansaharju@jmba.kielikone.local>
2013-02-27 19:08:26 +0200

    Added client boilerplate

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
 var mongoose = require('mongoose');
-var config = require('./config');
 var restify = require('restify');
 
 // setup mongoose & mongodb
-mongoose.connect(config.creds.mongoose_auth);
+mongoose.connect('mongodb://nodejitsu_trapridge:toujvd1dh5ioock6jtliel6lo4@ds051947.mongolab.com:51947/nodejitsu_trapridge_nodejitsudb1918183083');
 
 // create a mongoose schema to register a model with mongodb
 var MessageModel = mongoose.model('Message', new mongoose.Schema({
@@ -11,18 +10,24 @@ var MessageModel = mongoose.model('Message', new mongoose.Schema({
     date: Date
 })); 
 
-// setup & start server
+// setup server
 var server = restify.createServer();
+server.use(restify.gzipResponse());
 server.use(restify.bodyParser());
+server.pre(function(req, res, next) {
+    if(req.url === '/') req.url = '/index.html';
+    return next();
+});
 
-server.get('/', restify.serveStatic({
-    directory: './client',
-    default: 'index.html'
-}));
+// Set routes
 server.get('/api/v1/messages', getMessages);
 server.post('/api/v1/messages', postMessage);
 server.del('/api/v1/messages/:id', deleteMessage);
+server.get(/\/*/, restify.serveStatic({
+    directory: './client/app'
+}));
 
+// start server
 server.listen(8080, function() {
     console.log('%s listening at %s', server.name, server.url);
 });
